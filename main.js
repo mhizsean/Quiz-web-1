a4eee6a5e7a368bce7d807131d61ac7c67abc2e3
mhizsean <adeyiesther01@gmail.com>
2019-11-27 21:17:02 +0100

    added functionality

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 function check () {
-    
-}
+
+    let question1 = document.quiz.question1.value;
+    let question2 = document.quiz.question2.value;
+    let question3 = document.quiz.question3.value;
+    let correct = 0
+
+    if (question1 == 4) {
+        correct++
+    }
+
+    if (question2 == 7) {
+        correct++
+    }
+
+    if (question3 == "Donald Trump") {
+        correct++
+    }
+    document.getElementById("submit").style.visibility = "visible";
+    document.getElementById("correct").innerHTML = "You got " + correct + " correct";
+
+} 
